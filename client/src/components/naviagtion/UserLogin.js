d1b27cbae797fed2169df21d47131b6fe5d64494
suriya <suriyajagan25@gmail.com>
2022-12-15 08:37:59 +0530

    react form user register

---
+++ client/src/components/naviagtion/UserLogin.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const UserLogin = (props)=>{
+    return (
+        <div>
+            <h2>User UserLogin</h2>
+        </div>
+    )
+}
+
+export default UserLogin
---
+++ client/src/components/tenants/Tenants.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Tenants = (props)=>{
+    return (
+        <div>
+            <h2>Tenants</h2>
+        </div>
+    )
+}
+
+export default Tenants
