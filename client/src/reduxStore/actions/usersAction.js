306129465832d5d3ae60e9fb5d7f384689654e5b
suriya <suriyajagan25@gmail.com>
2022-12-15 10:41:39 +0530

    formik user register

--- client/src/reduxStore/actions/usersAction.js
+++ client/src/reduxStore/actions/usersAction.js
@@ -1,12 +1,13 @@
 import axios from "axios"
 
-export const startUserRegister = (requestData)=>{
+export const startUserRegister = (requestData,resetForm)=>{
     return (dispatch)=>{
         axios.post("http://localhost:3500/api/user/register",requestData)
             .then((response)=>{
                 const userData = response.data
                 console.log(userData)
-                dispatch( userRegister(data) )
+                dispatch( userRegister(userData) )
+                resetForm()
             })
             .catch((err)=>{
                 window.alert(`Register errors : ${err.message}`)
